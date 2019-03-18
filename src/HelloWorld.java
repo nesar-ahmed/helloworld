75efff0a97d373a7983d06496d6ebcd715945756
Nesar Ahmed <dev.nesar24@gmail.com>
2019-03-18 14:21:59 +0100

    add loop for print text

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args){
+
         System.out.println("Hello World");
+
         for(int i = 0; i < 10; i++){
             System.out.println("Line " +i);
+        }
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("Line One");
         }
     }
 }
